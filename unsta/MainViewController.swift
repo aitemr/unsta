17b80744432fa0a8dac2367e09e3159fa4ee7992
Islam Temirbek <temirbekia4@gmail.com>
2017-05-30 19:44:31 +0600

    feat(AppDelegate): add rootViewController

---
+++ unsta/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  unsta
+//
+//  Created by Islam on 30.05.17.
+//  Copyright © 2017 ZeroToOneLabs. All rights reserved.
+//
+
+import UIKit
+import Sugar
+import EasyPeasy
+
+class MainViewController: UIViewController {
+    
+    // MARK: View LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViews()
+        configureConstriants()
+    }
+    
+    // MARK: Configure Views
+    
+    func configureViews() {
+        
+    }
+    
+    // MARK: Configure Constraints
+    
+    func configureConstriants() {
+        
+    }
+
+}
+
